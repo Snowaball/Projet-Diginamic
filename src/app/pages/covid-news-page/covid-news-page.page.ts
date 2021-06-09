7919b2577f6042c3f75a8c858507761d66c85bcf
Marc Balouzet <marc.balouzet@outlook.fr>
2021-06-09 11:51:48 +0200

    toast working

--- src/app/pages/covid-news-page/covid-news-page.page.ts
+++ src/app/pages/covid-news-page/covid-news-page.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../../services/photo.service';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-covid-news-page',
   templateUrl: './covid-news-page.page.html',
@@ -7,12 +9,19 @@ import { PhotoService } from '../../services/photo.service';
 })
 export class CovidNewsPagePage implements OnInit {
 
-  constructor(public photoService: PhotoService) { }
-
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+  constructor(public photoService: PhotoService, public toastController: ToastController) {
   }
+
+  async addPhotoToGallery() {
+    await this.photoService.addNewToGallery();
+    const toast = await this.toastController.create({
+      color: 'primary',
+      message: 'Image ajoutée',
+      duration: 2000
+    });
+    await toast.present();
+  }
+
   ngOnInit() {
   }
-
 }
--- src/app/pages/ps5-news-page/ps5-news-page.page.ts
+++ src/app/pages/ps5-news-page/ps5-news-page.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../../services/photo.service';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-ps5-news-page',
@@ -8,10 +9,15 @@ import { PhotoService } from '../../services/photo.service';
 })
 export class Ps5NewsPagePage implements OnInit {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, public toastController: ToastController) { }
 
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+  async addPhotoToGallery() {
+    await this.photoService.addNewToGallery();
+    const toast = await this.toastController.create({
+      message: 'Image ajoutée',
+      duration: 2000
+    });
+    await toast.present();
   }
   ngOnInit() {
   }
